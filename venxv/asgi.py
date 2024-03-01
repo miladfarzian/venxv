cb3e383620d2577eaa3ee077b0675f2dd43a370a
miladfarzian <farziandoc@gmail.com>
2024-03-01 04:50:48 +0330

    create django app

---
+++ venxv/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for venxv project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "venxv.settings")
+
+application = get_asgi_application()
